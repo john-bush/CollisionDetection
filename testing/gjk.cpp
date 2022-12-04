a5801c39a0407be90c1e395972f6df46c27b3bb2
baloth325 <sethggoolsby@gmail.com>
2022-12-04 14:23:51 -0800

    Seth 2

--- testing/gjk.cpp
+++ testing/gjk.cpp
@@ -442,20 +442,51 @@ vector<Point2> generateRandomPolygon(int n, int sizeX, int sizeY, float shiftX =
     return polygon;
 }
 
-void printToFile(Point2 *polygon1, int length1)
+vector<vector<Point2>> divide_into_sub_polygons(vector<Point2> Poly, int num)
+{
+    vector<vector<Point2>> subPoly;
+
+    for(int x = 0; x < num; x++)
+    {
+        vector<Point2> sub = {Poly.begin() + x, Poly.begin()+ x + 1};
+        subPoly.push_back(sub);
+    }
+    return subPoly;
+}
+
+void printToFile(Point2 *polygon1, int length1, std::string fileName)
 {
     std::fstream object1;
     int num_obj = 0;
-    std::string fileName = "../Point_Clouds/object_";
-
+    
+    object1.open(fileName);
     //creates a unique identifier for each object
-    struct std::stat buffer;
+    /* struct std::stat buffer;
     while(stat ((fileName + std::to_string(num_obj)).c_str(), &buffer) == 0)
     {
         num_obj ++;
-    }
+    }*/
+   for (int x = 0; x < length1; x++)
+   {
+    object1 << polygon1[x].x << " " <<polygon1[x].y << std::endl;
+   }
+   object1.close();
+}
+vector<Point2> readFromFile(std::string fileName)
+{
+    vector<Point2> polygon;
+    std::fstream object1;
+    std::string line;
+    object1.open(fileName);
 
-    randomPoints = out;
+    while(std::getline(object1,line))
+    {
+        Point2 newPoint;
+        newPoint.x = line.substr(0,line.find(" "));
+        newPoint.y = line.substr(line.find(" "));
+        polygon.push_back(newPoint);
+    }
+return polygon;
 }
 
 /**
